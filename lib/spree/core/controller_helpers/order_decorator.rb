9449f3307e586e5b6a0fb7cb52e13bb58197e714
Matthew Kennedy <m.kennedy@me.com>
2018-06-19 17:52:10 +0100

    Remove randmon hash

--- lib/spree/core/controller_helpers/order_decorator.rb
+++ lib/spree/core/controller_helpers/order_decorator.rb
@@ -1,7 +1,6 @@
 Spree::Core::ControllerHelpers::Order.class_eval do
   def current_currency
     # ensure session currency is supported
-    #
     if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
       session[:currency]
     else
