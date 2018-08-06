21edafbab3f41ff825fd116d61e2ebdeeb7695df
Matthew Kennedy <m.kennedy@me.com>
2018-08-07 00:05:23 +0100

    remove else

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -28,15 +28,13 @@ module Spree
         session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
       end
 
-    else
-
       if params[:currency].present?
         @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
         # Make sure that we update the current order, so the currency change is reflected.
         current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
         session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
       end
-      
+
     end
   end
 end
