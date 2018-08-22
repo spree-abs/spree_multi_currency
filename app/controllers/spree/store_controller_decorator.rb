34602f4792fd92692a03690ab46b7b9e037de3b4
Matthew Kennedy <m.kennedy@me.com>
2018-08-22 22:12:44 +0100

    use elsif

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -3,39 +3,38 @@ module Spree
     before_action :set_locale
 
     def set_locale
+
       if (cookies[:returning].blank?)
+
             if locale == :'en-GB'
               params[:currency] = "GBP"
               @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-              # Make sure that we update the current order, so the currency change is reflected.
               current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
               session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-            end
 
-            if locale == :'en-AU'
+            elsif locale == :'en-AU'
               params[:currency] = "AUD"
               @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-              # Make sure that we update the current order, so the currency change is reflected.
               current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
               session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-            end
 
-            if locale == :'en-US'
+            elsif locale == :'en-US'
               params[:currency] = "USD"
               @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-              # Make sure that we update the current order, so the currency change is reflected.
               current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
               session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
             end
+            
       end
-      
+
       if params[:currency].present?
         @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-        # Make sure that we update the current order, so the currency change is reflected.
         current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
         session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
       end
 
     end
+
   end
+
 end
