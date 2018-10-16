ec83580733b22705d011762a8e623d9a197b8705
Matthew Kennedy <m.kennedy@me.com>
2018-10-16 17:38:00 +0100

    add locations

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -2,29 +2,75 @@ module Spree
   StoreController.class_eval do
     before_action :set_locale
 
+    def visitor_location
+      euro_countries = [ 'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'EL', 'HU', 'IE', 'IT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SV' ]
+      if euro_countries.include? request.location.country_code.to_s
+        visitor_location = 'EUR'
+      else
+        visitor_location = request.location.country_code
+      end
+    end
+
     def set_locale
 
       if (cookies[:returning].blank?)
 
-            if locale == :'en-GB'
-              params[:currency] = "GBP"
-              @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-              current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-              session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+          if locale == I18n.default_locale && request.location.country_code.present?
+              if visitor_location == "EUR"
+                params[:currency] = visitor_location
+                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
 
-            elsif locale == :'en-AU'
-              params[:currency] = "AUD"
-              @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-              current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-              session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+              elsif visitor_location == "GB"
+                params[:currency] = "GBP"
+                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
 
-            elsif locale == :'en-US'
-              params[:currency] = "USD"
-              @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-              current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-              session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-            end
-            
+              elsif @visitor_location == "AU"
+                params[:currency] = "AUD"
+                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+
+              elsif @visitor_location == "CA"
+                params[:currency] = "CAD"
+                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+
+              elsif
+                params[:currency] = "USD"
+                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+              end
+
+          elsif locale == :'en-GB'
+            params[:currency] = "GBP"
+            @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+            current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+            session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+
+          elsif locale == :'en-AU'
+            params[:currency] = "AUD"
+            @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+            current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+            session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+
+          elsif locale == :'en-CA'
+            params[:currency] = "CAD"
+            @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+            current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+            session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+
+          elsif locale == :'en-US'
+            params[:currency] = "USD"
+            @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+            current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+            session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+          end
       end
 
       if params[:currency].present?
@@ -36,5 +82,4 @@ module Spree
     end
 
   end
-
 end
