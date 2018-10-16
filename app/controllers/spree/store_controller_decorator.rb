2aa83008733ac6bc319e8c8ecf1b5dc119514521
Matthew Kennedy <m.kennedy@me.com>
2018-10-16 20:10:06 +0100

    try that

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -2,20 +2,18 @@ module Spree
   StoreController.class_eval do
     before_action :set_locale
 
-    def visitor_location
+    def set_locale
+
       euro_countries = [ 'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'EL', 'HU', 'IE', 'IT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SV' ]
       if euro_countries.include? request.location.country_code.to_s
         visitor_location = 'EUR'
       else
         visitor_location = request.location.country_code
       end
-    end
-
-    def set_locale
 
       if (cookies[:returning].blank?)
 
-          if locale == I18n.default_locale && request.location.country_code.present?
+          if locale == I18n.default_locale && request.location.country_code.present? && !browser.bot?
               if visitor_location == "EUR"
                 params[:currency] = visitor_location
                 @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
@@ -73,6 +71,7 @@ module Spree
           end
       end
 
+
       if params[:currency].present?
         @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
         current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
@@ -80,6 +79,5 @@ module Spree
       end
 
     end
-
   end
 end
--- spree_multi_currency.gemspec
+++ spree_multi_currency.gemspec
@@ -25,6 +25,7 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'spree_backend', '>= 3.1.0', '< 4.0'
   s.add_runtime_dependency 'spree_extension'
   s.add_runtime_dependency 'geocoder'
+  s.add_runtime_dependency 'browser'
 
   s.add_development_dependency 'appraisal'
   s.add_development_dependency 'capybara'
