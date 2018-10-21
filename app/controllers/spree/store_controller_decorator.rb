a57f95d2dbf4e9a64799b2162bd9e565a5c0ab30
Matthew Kennedy <m.kennedy@me.com>
2018-10-21 09:06:36 +0100

    update controler to be more clean

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -3,73 +3,51 @@ module Spree
     before_action :set_locale
 
     def set_locale
-        euro_countries = [ 'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'EL', 'HU', 'IE', 'IT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SV' ]
-        # Only Run code once so that user can change currency if they wish
-        if (cookies[:returning].blank?)
 
-# START If website is set to default location and the users country code is not nill and is not a bot.
+      # Set Euro zone country list.
+      euro_countries = [ 'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'EL', 'HU', 'IE', 'IT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SV' ]
+
+      # Only Run code once so that user can change currency if they wish
+      if (cookies[:returning].blank?)
+
+          # START If website is set to the default location and the users country code is not nill and the visitor is not a bot.
           if locale == I18n.default_locale && !request.location.country_code.nil? && !browser.bot?
 
-            if euro_countries.include? request.location.country_code.to_s
-              visitor_location = 'EUR'
-            else
-              visitor_location = request.location.country_code
-            end
+                  # Test for Euro zone country
+                  if euro_countries.include? request.location.country_code.to_s
+                    visitor_location = 'EUR'
+                    else
+                    visitor_location = request.location.country_code
+                  end
 
-              if visitor_location == "EUR"
-                params[:currency] = visitor_location
-                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-
-              elsif visitor_location == "GB"
-                params[:currency] = "GBP"
-                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-
-              elsif @visitor_location == "AU"
-                params[:currency] = "AUD"
-                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-
-              elsif @visitor_location == "CA"
-                params[:currency] = "CAD"
-                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-
-              elsif
-                params[:currency] = "USD"
-                @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-                current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-                session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-              end
-# Else respond to the url local and set currency to match
-          elsif locale == :'en-GB'
-            params[:currency] = "GBP"
-            @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-            current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-            session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-
-          elsif locale == :'en-AU'
-            params[:currency] = "AUD"
-            @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-            current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-            session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-
-          elsif locale == :'en-CA'
-            params[:currency] = "CAD"
-            @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-            current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-            session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
-
-          elsif locale == :'en-US'
-            params[:currency] = "USD"
-            @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
-            current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
-            session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+                  if visitor_location == "EUR"
+                    params[:currency] = visitor_location
+                    elsif visitor_location == "GB"
+                      params[:currency] = "GBP"
+                    elsif @visitor_location == "AU"
+                      params[:currency] = "AUD"
+                    elsif @visitor_location == "CA"
+                      params[:currency] = "CAD"
+                  end
+            # Else respond to the url local and set currency to match
+            elsif locale == :'en-GB'
+              params[:currency] = "GBP"
+            elsif locale == :'de'
+              params[:currency] = "EUR"
+            elsif locale == :'fr'
+              params[:currency] = "EUR"
+            elsif locale == :'it'
+              params[:currency] = "EUR"
+            elsif locale == :'es'
+              params[:currency] = "EUR"
+            elsif locale == :'sv'
+              params[:currency] = "EUR"
+            elsif locale == :'en-AU'
+              params[:currency] = "AUD"
+            elsif locale == :'en-CA'
+              params[:currency] = "CAD"
+            elsif locale == :'en-US'
+              params[:currency] = "USD"
           end
       end
 
