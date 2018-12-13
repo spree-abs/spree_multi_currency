be3b430a19d11ccd3e1af085b88a63eebf635bbe
Matthew Kennedy <m.kennedy@me.com>
2018-12-13 19:28:01 +0000

    make it simple

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -4,46 +4,17 @@ module Spree
     before_action :set_locale
 
       def set_locale
-
-        # Define a list of countires that use Euros.
-        euro_zone_countries = [ 'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'EL',
-                              'HU', 'IE', 'IT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SV' ]
-
         if current_store.code != current_currency
-
-          # IF the store is loaded with the default store code, and the visitor is not a bot and the geo code has not been set.
-          if current_store.code == 'spree' && !browser.bot? && (cookies[:cf_geo_currency].blank?)
-
-              # IF the visitor is located in a Euro Zone country (EZ)
-              if euro_zone_countries.include? request.headers["CF-IPCountry"].to_s
-                visitor_location = "EZ"
-                else
-                visitor_location = request.headers["CF-IPCountry"].to_s
-              end
-
-              case visitor_location
-                when 'EZ'
-                  Spree::Config[:currency] = 'EUR'
-                when 'GB'
-                  Spree::Config[:currency] = 'GBP'
-                else
-                  Spree::Config[:currency] = 'USD'
-              end
-              (cookies[:cf_geo_currency] = current_currency)
-          # ELSE check for CURRENCY in STORE CODE
-          else
               case current_store.code
                 when 'EUR'
                   Spree::Config[:currency] = 'EUR'
                 when 'GBP'
                   Spree::Config[:currency] = 'GBP'
-                when 'USD'
+                when 'else'
                   Spree::Config[:currency] = 'USD'
               end
-          end
         end
-        
-    end
+      end
 
   end
 end
